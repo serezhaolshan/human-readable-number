79086c707c21c12ff9c777593ed387c7188fb8c1
Serhii Olshanivskyi <50380528+serezhaolshan@users.noreply.github.com>
2021-02-05 12:20:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
+
+  if (!number || number == 0) return 'zero';
+
+  let result = '', dig = 0;
+
+  const digits = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'];
   
-}
+  if ( number >= 100 ) { 
+      dig = (number/100)|0;
+      result = `${ digits[dig] } hundred`; 
+      number -= dig * 100; 
+      }
+
+  if ( number > 19  ) {
+
+	 const tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+
+      dig = (number/10)|0;
+			number -= dig * 10;
+      result = `${result} ${tens[dig]} ${digits[number]}`;      
+      } 
+			else result = `${result} ${digits[number]}`; 
+
+  return result.trim();
+} 
